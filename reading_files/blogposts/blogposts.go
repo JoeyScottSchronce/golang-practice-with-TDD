6ac05153f75cf435913e731326594bc7a443e7e3
JoeyScottSchronce <joeyscottschronce@gmail.com>
2025-10-27 09:52:43 -0400

    updated

--- reading_files/blogposts/blogposts.go
+++ reading_files/blogposts/blogposts.go
@@ -1,1 +1,19 @@
 package blogposts
+
+import (
+	"io/fs"
+)
+
+type Post struct {
+}
+
+func NewPostsFromFS(filesystem fs.FS) []Post {
+	dir, _ := fs.ReadDir(filesystem, ".")
+	var posts []Post
+
+	for range dir {
+		posts = append(posts, Post{})
+	}
+
+	return posts
+}
