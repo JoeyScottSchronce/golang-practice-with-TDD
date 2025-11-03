2e28f6c1938a4293b5d55a8393c0c08761d60de3
JoeyScottSchronce <joeyscottschronce@gmail.com>
2025-11-03 08:08:46 -0500

    generics

---
+++ generics/generic_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestAssertFunctions(t *testing.T) {
+	t.Run("asserting on intergers", func(t *testing.T) {
+		AssertEqual(t, 1, 1)
+		AssertNotEqual(t, 1, 2)
+	})
+
+	t.Run("assert on strings", func(t *testing.T) {
+		AssertEqual(t, "hello", "hello")
+		AssertNotEqual(t, "hello", "not hello")
+	})
+}
