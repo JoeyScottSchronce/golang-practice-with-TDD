a83d51f5324fb5df1e61bd36323e33b3e45749e1
JoeyScottSchronce <joeyscottschronce@gmail.com>
2025-11-10 14:38:26 -0500

    refactored

--- arrays-and-slices/arrays_and_slices.go
+++ arrays-and-slices/arrays_and_slices.go
@@ -1,29 +1,55 @@
 package main
 
 func Sum(arr []int) int {
-	var result int
-	for _, v := range arr {
-		result += v
-	}
-	return result
+	add := func(y, x int) int { return y + x }
+	return Reduce(arr, add, 0)
 }
 
 func SumAll(slices ...[]int) []int {
-	result := make([]int, len(slices))
-	for i, arr := range slices {
-		result[i] = Sum(arr)
+
+	addAll := func(arr, x []int) []int {
+		return append(arr, Sum(x))
+	}
+	return Reduce(slices, addAll, []int{})
+}
+
+func SumAllTails(arr ...[]int) []int {
+	sumTail := func(acc, x []int) []int {
+		if len(x) == 0 {
+			return append(acc, 0)
+		} else {
+			tail := x[1:]
+			return append(acc, Sum(tail))
+		}
+	}
+	return Reduce(arr, sumTail, []int{})
+}
+
+func Reduce[T any](collection []T, f func(T, T) T, initialValue T) T {
+	var result = initialValue
+
+	for _, v := range collection {
+		result = f(result, v)
 	}
 	return result
 }
 
-func SumAllTails(arr ...[]int) []int {
-	result := make([]int, len(arr))
-	for i, v := range arr {
-		if len(v) == 0 {
-			result[i] = 0
-		} else {
-			result[i] = Sum(v[1:])
+type Transaction struct {
+	From string
+	To   string
+	Sum  float64
+}
+
+func BalanceFor(transactions []Transaction, name string) float64 {
+	var balance float64
+
+	for _, t := range transactions {
+		if t.From == name {
+			balance -= t.Sum
+		}
+		if t.To == name {
+			balance += t.Sum
 		}
 	}
-	return result
+	return balance
 }
--- arrays-and-slices/arrays_and_slices_test.go
+++ arrays-and-slices/arrays_and_slices_test.go
@@ -53,3 +53,55 @@ func TestSum(t *testing.T) {
 		}
 	})
 }
+
+func AssertEqual[T comparable](t *testing.T, got, want T) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
+func AssertNotEqual[T comparable](t *testing.T, got, want T) {
+	t.Helper()
+
+	if got == want {
+		t.Errorf("didn't want %v", got)
+	}
+}
+
+func TestReduce(t *testing.T) {
+	t.Run("multiplication of all elements", func(t *testing.T) {
+		multiply := func(x, y int) int {
+			return x * y
+		}
+
+		AssertEqual(t, Reduce([]int{1, 2, 3}, multiply, 1), 6)
+	})
+
+	t.Run("concatenate strings", func(t *testing.T) {
+		concatenate := func(x, y string) string {
+			return x + y
+		}
+
+		AssertEqual(t, Reduce([]string{"a", "b", "c"}, concatenate, ""), "abc")
+	})
+}
+func TestBadBank(t *testing.T) {
+	transactions := []Transaction{
+		{
+			From: "Chris",
+			To:   "Riya",
+			Sum:  100,
+		},
+		{
+			From: "Tam",
+			To:   "Chris",
+			Sum:  25,
+		},
+	}
+
+	AssertEqual(t, BalanceFor(transactions, "Riya"), 100)
+	AssertEqual(t, BalanceFor(transactions, "Chris"), -75)
+	AssertEqual(t, BalanceFor(transactions, "Tam"), -25)
+}
